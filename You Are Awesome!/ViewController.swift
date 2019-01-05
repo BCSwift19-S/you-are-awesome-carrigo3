66920cb0b57139fa0d1cc14b3b510fbc0da697c1
Cam Arrigo <carrigo3@gmail.com>
2019-01-05 17:29:37 -0500

    2.13 Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -27,28 +27,15 @@ class ViewController: UIViewController {
                         "You've got the design skills of Jony Ive",
                         "I can't wait to download your app!"
                         ]
+        //Makes sure that a random message shows each time without repeats
+        var newIndex = -1
+        
+        repeat {
+            newIndex = Int.random(in: 0..<messages.count)
+        } while index == newIndex
+        index = newIndex
+        messageLabel.text = messages[index]
 
-        messageLabel.text = messages.randomElement()!
-        
-//        if index == messages.count - 1{
-//            index = 0
-//        } else {
-//            index += 1
-//        }
-        
-        
-        
-//        let message1 = "You Are Awesome!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        } else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        } else {
-//            messageLabel.text = message1
-//        }
     }
 
 }
